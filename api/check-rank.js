8163898bd3c60556ab109185699ca23fa1671cff
Amarjeet Kumar ( Alan ) <140792682+amarjeetkumar01-stack@users.noreply.github.com>
2026-02-27 11:19:28 +0530

    add api

--- api/check-rank.js
+++ api/check-rank.js
@@ -1,1 +1,14 @@
+export default async function handler(req, res) {
+  const email = req.query.email;
 
+  const GOOGLE_SCRIPT_URL = "PASTE_SCRIPT_URL_HERE";
+
+  try {
+    const response = await fetch(`${GOOGLE_SCRIPT_URL}?email=${email}`);
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch rank" });
+  }
+}
